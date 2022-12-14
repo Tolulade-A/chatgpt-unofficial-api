39c0536c4779c04e9fcb6e8a23090a350c52d6ca
Tolulade Ademisoye <princemaxsteel10@gmail.com>
2022-12-14 13:29:03 +0100

    Modified chatpgpt unofficial api.

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# chatgpt-unofficial-api
+# ChatGPT Unofficial api
+
+* It uses playwright and firefox to open browser and parse html.
+* It is an unoffical api for development purpose only.
+
+```
+Playwright comes with three web browsers to my knowledge;
+
+* Chromiun - Chrome
+* Firefox
+* Webkit
+```
+
+# How to install
+
+* Make sure that python and virtual environment is installed. In my case, i'm using PyCharm.
+
+* Create a new virtual environment
+
+```python
+# one time
+virtualenv -p $(which python3) pyenv
+
+# everytime you want to run the server
+source pyenv/bin/activate
+
+## in my case, I used a virtual environment in pycharm, so i didn't use the two steps above.
+```
+
+* Now install the requirements
+
+```
+pip install -r requirements.txt
+
+PyCharm should automatically notify you to install this, if it finds the file in the project env.
+```
+
+* If you are installing playwright for the first time, it will ask you to run this command for one time only.
+
+```
+playwright install or
+
+ms-playwright install (if playwright doesn't work)
+
+Playwright didn't work for me so I used `ms-playwrigt` which worked well.
+```
+
+* Now run the server
+
+```
+python server.py or
+
+python file_path of server.py on your pc/dev env.
+```
+
+* The server runs at port `5000`. If you want to change, you can change it in server.py
+
+
+# Api Documentation
+
+* There is a single end point only. It is available at `/chat`
+
+```sh
+curl -XGET http://localhost:5001/chat?q=Write%20a%20python%20program%20to%20reverse%20a%20list
+```
+
+# Updates
+
+* [8 Dec 2022]: Updated parsing logic (credits @CoolLoong)
+
+# Credit
+
+* All the credit for this script goes to [Daniel Gross's whatsapp gpt](https://github.com/danielgross/whatsapp-gpt) package & [Taranjeet Singh](https://github.com/taranjeet/chatgpt-api) who made the code simplified for Python developers . I have just taken the script as an individual file like Taranjeet Singh did and added documentation for how to install and run it like he did.
---
+++ server.py
@@ -0,0 +1,138 @@
+"""Make some requests to OpenAI's chatbot"""
+#tips https://playwright.tech/blog/end-to-end-tests-with-playwright-python
+
+import time
+import os
+import flask
+
+from flask import g
+
+
+from playwright.sync_api import sync_playwright
+
+APP = flask.Flask(__name__)
+PLAY = sync_playwright().start()
+BROWSER = PLAY.firefox.launch_persistent_context(
+    user_data_dir='C:XXX\\XXX\\AppData\\Local\\ms-playwright', #location of the playwright installation
+    #executable_path= ('C:\\XXX\XXX\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe'), #if you want to change
+    #firefox to chromium.
+    headless=False,
+)
+PAGE = BROWSER.new_page()
+
+
+"""
+from playwright.sync_api import sync_playwright
+
+APP = flask.Flask(__name__)
+user_data_dir = 'C:Users\\Reispar_Tolulade\\AppData\\Local\\ms-playwright'
+#user_data_dir = os.path.join(os.getcwd(), user_data_dir)
+
+with sync_playwright() as PLAY:
+    BROWSER = PLAY.chromium.launch_persistent_context(
+        user_data_dir,
+        executable_path=('C:\\XXX\XXX\\AppData\\Local\\Google\\Chrome\\Application\\new_chrome.exe'),
+        headless=False)
+    PAGE = BROWSER.new_page()
+    PAGE.goto("https://chat.openai.com/")
+    #BROWSER.close()
+"""
+
+
+def get_input_box():
+    """Get the child textarea of `PromptTextarea__TextareaWrapper`"""
+    return PAGE.query_selector("textarea")
+
+def is_logged_in():
+    # See if we have a textarea with data-id="root"
+    return get_input_box() is not None
+
+def is_loading_response() -> bool:
+    """See if the send button is diabled, if it does, we're not loading"""
+    return not PAGE.query_selector("textarea ~ button").is_enabled()
+
+def send_message(message):
+    # Send the message
+    box = get_input_box()
+    box.click()
+    box.fill(message)
+    box.press("Enter")
+
+def get_last_message():
+    """Get the latest message"""
+    while is_loading_response():
+        time.sleep(0.25)
+    page_elements = PAGE.query_selector_all("div[class*='request-:']")
+    last_element = page_elements.pop()
+    return last_element.inner_text()
+
+def regenerate_response():
+    """Clicks on the Try again button.
+    Returns None if there is no button"""
+    try_again_button = PAGE.query_selector("button:has-text('Try again')")
+    if try_again_button is not None:
+        try_again_button.click()
+    return try_again_button
+
+def get_reset_button():
+    """Returns the reset thread button (it is an a tag not a button)"""
+    return PAGE.query_selector("a:has-text('Reset thread')")
+
+@APP.route("/chat", methods=["GET"]) #TODO: make this a POST
+def chat():
+    message = flask.request.args.get("q")
+    print("Sending message: ", message)
+    send_message(message)
+    response = get_last_message()
+    print("Response: ", response)
+    return response
+
+# create a route for regenerating the response
+@APP.route("/regenerate", methods=["POST"])
+def regenerate():
+    print("Regenerating response")
+    if regenerate_response() is None:
+        return "No response to regenerate"
+    response = get_last_message()
+    print("Response: ", response)
+    return response
+
+@APP.route("/reset", methods=["POST"])
+def reset():
+    print("Resetting chat")
+    get_reset_button().click()
+    return "Chat thread reset"
+
+@APP.route("/restart", methods=["POST"])
+def restart():
+    global PAGE,BROWSER,PLAY
+    PAGE.close()
+    BROWSER.close()
+    PLAY.stop()
+    time.sleep(0.25)
+    PLAY = sync_playwright().start()
+    BROWSER = PLAY.firefox.launch_persistent_context(
+        user_data_dir='C:XXX\\XXX\\AppData\\Local\\ms-playwright',
+        #executable_path=('C:\\XXX\XXX\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe'),
+        headless=False,
+    )
+    PAGE = BROWSER.new_page()
+    PAGE.goto("https://chat.openai.com/")
+    return "API restart!"
+
+
+def start_browser():
+    PAGE.goto("https://chat.openai.com/")
+    if not is_logged_in():
+        print("Please log in to OpenAI Chat")
+        print("Press enter when you're done")
+        input()
+    else:
+        print("Logged in")
+        APP.run(host='127.0.0.1', port=5000, threaded=False) #or
+        #APP.run(port=5000, threaded=False) #can also be used
+
+if __name__ == "__main__":
+    start_browser()
+
+
